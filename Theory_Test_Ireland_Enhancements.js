c0ea753c687a41d383e067a3e78e31740300f685
Lyushen <L3122114@gmail.com>
2023-09-16 23:30:01 +0100

    update file name

--- Theory_Test_Ireland_Enhancements.js
+++ Theory_Test_Ireland_Enhancements.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Theory Test Ireland Enhancements
 // @namespace    http://tampermonkey.net/
-// @version      1.001
+// @version      1.002
 // @description  Several enhancements for Theory Test Ireland site, such as autoplay, roll-up top bar for more space and block useless elements
 // @author       Lyushen
 // @match        https://theorytestireland.org/*
