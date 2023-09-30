0bf8cc61d40e23170a49525411a13cedb61678b7
Lyushen <78591028+Lyushen@users.noreply.github.com>
2023-09-30 16:41:22 +0100

    words update

--- 4pda_Optimisations.js
+++ 4pda_Optimisations.js
@@ -4,7 +4,7 @@
 // @namespace    https://github.com/Lyushen
 // @author       Lyushen
 // @license      GNU
-// @version      1.00212
+// @version      1.00213
 // @description  Block specific first elements from 4pda.to
 // @homepageURL  https://github.com/Lyushen/TMEnchancments
 // @supportURL   https://github.com/Lyushen/TMEnchancments/issues
@@ -44,8 +44,9 @@
         "РФ",
         "VK",
         "MTC",
-        "Lada",,
-        "МТС"
+        "Lada",
+        "МТС",
+        "Госдум"
     ];
 
     let cssRules = keywords.map(keyword => `article[class^="post"]:has(div>h2>a[title*="${keyword}"]) { display: none !important; }`).join(' ');
