42b9bfc546fee476b9133d44891599a291b3936d
Lyushen <78591028+Lyushen@users.noreply.github.com>
2023-11-16 21:35:21 +0000

    Update 4pda_Optimisations.js

--- 4pda_Optimisations.js
+++ 4pda_Optimisations.js
@@ -4,7 +4,7 @@
 // @namespace    https://github.com/Lyushen
 // @author       Lyushen
 // @license      GNU
-// @version      1.00214
+// @version      1.00215
 // @description  Block specific first elements from 4pda.to
 // @homepageURL  https://github.com/Lyushen/TMEnchancments
 // @supportURL   https://github.com/Lyushen/TMEnchancments/issues
@@ -30,6 +30,7 @@
         "VK Видео",
         "Смута",
         "Смуты",
+        "Смуте",
         "Слух:",
         "Инсайд:",
         "Инсайды",
