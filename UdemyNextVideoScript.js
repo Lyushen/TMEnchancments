e215eb3c1e095eef6efea1fa982f76bd24a9d55d
Lyushen <78591028+Lyushen@users.noreply.github.com>
2024-04-24 16:23:08 +0100

    upd the popup window

--- UdemyNextVideoScript.js
+++ UdemyNextVideoScript.js
@@ -4,7 +4,7 @@
 // @namespace    https://github.com/Lyushen
 // @author       Lyushen
 // @license      GNU
-// @version      1.006
+// @version      1.007
 // @description  This script presses the Next element that will switch to a new video when it's about to end. Tracks video progress and triggers a button click near the end, with notifications.
 // @homepageURL  https://github.com/Lyushen/TMEnchancments
 // @supportURL   https://github.com/Lyushen/TMEnchancments/issues
@@ -22,6 +22,7 @@
     const startThreshold = 3; // Start monitoring after 3 seconds of playback
 
     function showNotification(message) {
+        console.log(message);
         const existingNotification = document.getElementById('gm-notification');
         if (existingNotification) {
             document.body.removeChild(existingNotification);
@@ -33,11 +34,11 @@
         notification.style.top = '45%';
         notification.style.right = '50%';
         notification.style.background = 'rgba(255, 255, 255, 0.9)';
-        notification.style.border = '1px solid #ccc';
+        notification.style.border = '2px solid #000';
         notification.style.borderRadius = '5px';
         notification.style.padding = '10px';
         notification.style.zIndex = '10001';
-        notification.style.transition = 'opacity 1s';
+        notification.style.transition = 'opacity 2s';
         notification.innerText = message;
 
         document.body.appendChild(notification);
