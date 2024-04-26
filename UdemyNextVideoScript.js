7dd4741cf8f9be63de2d3d2891a4fa9ba6e85bb9
Lyushen <78591028+Lyushen@users.noreply.github.com>
2024-04-26 14:05:51 +0100

    upd version

--- UdemyNextVideoScript.js
+++ UdemyNextVideoScript.js
@@ -4,7 +4,7 @@
 // @namespace    https://github.com/Lyushen
 // @author       Lyushen
 // @license      GNU
-// @version      1.014
+// @version      1.015
 // @description  This script presses the Next element that will switch to a new video when it's about to end. Tracks video progress and triggers a button click near the end, with notifications.
 // @homepageURL  https://github.com/Lyushen/TMEnchancments
 // @supportURL   https://github.com/Lyushen/TMEnchancments/issues
