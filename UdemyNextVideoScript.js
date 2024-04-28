d9f7ba1947da01ce60343547193083fa3a801a44
Lyushen <78591028+Lyushen@users.noreply.github.com>
2024-04-28 16:43:35 +0100

    a

--- UdemyNextVideoScript.js
+++ UdemyNextVideoScript.js
@@ -20,23 +20,23 @@
     const thresholdSeconds = 3; // Trigger 5 seconds before the video ends
     const startThreshold = 3; // Start monitoring after 3 seconds of playback
     const notificationLeadTime = 5; // Notification appears 2 seconds before the action, in milliseconds
-    
+
     function monitorVideo() {
         const videoElement = document.querySelector('[role="slider"][data-purpose="video-progress-bar"]');
         if (!videoElement) return;
-    
+
         const ariaValueText = videoElement.getAttribute('aria-valuetext');
         if (!ariaValueText) return;
-    
+
         const parts = ariaValueText.split(' of ');
         if (parts.length !== 2) return;
-    
+
         const currentTime = parseTime(parts[0].trim());
         const totalTime = parseTime(parts[1].trim());
-    
+
         // Check if it's time to start monitoring the video playback
         if (currentTime < startThreshold) return;
-    
+
         // Display notification at the specified lead time before the video ends
         if ((totalTime - currentTime) <= (thresholdSeconds + notificationLeadTime) && !videoElement.notificationShown) {
             videoElement.notificationShown = true; // Set a flag to ensure notification is shown only once
@@ -44,7 +44,7 @@
             //showNotification(`Next Video in ${notificationLeadTime}s`, notificationLeadTime);
             countdownPopUp(3)
         }
-    
+
         // Trigger the button click and manage the button state based on video progress
         const button = document.querySelector('[role="link"][data-purpose="go-to-next"]');
         if (button) {
@@ -55,7 +55,7 @@
                 button.disabled = true; // Immediately disable the button to prevent multiple clicks
                 videoElement.lastClickTime = currentTime; // Record the last click time
             }
-    
+
             // Check if the currentTime has advanced at least 2 seconds from the last click time, then re-enable
             if (button.disabled && videoElement.lastClickTime && (currentTime - videoElement.lastClickTime >= 2)) {
                 button.disabled = false;
@@ -91,7 +91,7 @@
             }
         `, style.sheet.cssRules.length);
     }
-    
+
     function showNotification(message, duration = 1000) {
         const notification = document.createElement('div');
         notification.className = 'notification';
@@ -104,7 +104,7 @@
             document.body.removeChild(notification);
         });
     }
-    
+
     function countdownPopUp(duration) {
         let countdown = duration;
         const interval = setInterval(() => {
@@ -115,7 +115,6 @@
             }
         }, 1000);
     }
-    
 
     function parseTime(timeStr) {
         const timeParts = timeStr.split(':').map(Number);
