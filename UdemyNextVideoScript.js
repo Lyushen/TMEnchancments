8f878ed22842c015d8ef79914ebca8e229602df3
Lyushen <78591028+Lyushen@users.noreply.github.com>
2024-04-28 17:22:33 +0100

    didn't load? why

--- UdemyNextVideoScript.js
+++ UdemyNextVideoScript.js
@@ -4,7 +4,7 @@
 // @namespace    https://github.com/Lyushen
 // @author       Lyushen
 // @license      GNU
-// @version      1.025
+// @version      1.026
 // @description  This script presses the Next element that will switch to a new video when it's about to end. Tracks video progress and triggers a button click near the end, with notifications.
 // @homepageURL  https://github.com/Lyushen/TMEnchancments
 // @supportURL   https://github.com/Lyushen/TMEnchancments/issues
@@ -157,5 +157,6 @@
     addNotificationStyles();
     // Setup interval and initial notification
     setInterval(monitorVideo, checkInterval);
+    console.log(`Script Loaded`)
     showNotification("Script Loaded");
 })();
