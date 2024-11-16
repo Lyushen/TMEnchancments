4fac3d1de160ec818e94e11d54752eed036c4a43
Lyushen <L3122114@gmail.com>
2024-11-16 15:41:50 +0000

    a

--- rsa_availability.js
+++ rsa_availability.js
@@ -2,7 +2,7 @@
 // @name         RSA Availability Checker
 // @namespace    http://tampermonkey.net/
 // @icon         https://www.google.com/s2/favicons?sz=128&domain=https://rsa.ie
-// @version      1.341
+// @version      1.342
 // @description  Automatically navigates through rsa.ie and myroadsafety.rsa.ie to check availability slots.
 // @author       Lyushen
 // @license      GNU
